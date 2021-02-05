59321f6901448b5f2c380953e458467a01fe0fef
Or Shachar <orsh>
2021-02-05 15:42:15 +0200

    Initial commit

---
+++ FakeNewsDetection/check_requirements.py
@@ -0,0 +1,25 @@
+# Libraries
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+
+# Preliminaries
+
+from torchtext.data import Field, TabularDataset, BucketIterator, Iterator
+
+# Models
+
+import torch.nn as nn
+from transformers import BertTokenizer, BertForSequenceClassification
+
+# Training
+
+import torch.optim as optim
+
+# Evaluation
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import seaborn as sns
+
+print("all dependencies installed")
