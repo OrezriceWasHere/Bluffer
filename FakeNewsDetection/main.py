475c5dbf82250f8c50b559529ba73afd574630e6
Or Shachar <orsh>
2021-02-14 15:40:12 +0200

    changing parameters

--- FakeNewsDetection/main.py
+++ FakeNewsDetection/main.py
@@ -5,7 +5,7 @@ import torch.optim as optim
 
 model = BERT()
 model = model.to(Parameters.DEVICE)
-optimizer = optim.Adam(model.parameters(), lr=2e-5)
+optimizer = optim.Adam(model.parameters(), lr=2e-4)
 
-train(model=model, optimizer=optimizer)
+train(model=model, optimizer=optimizer, num_epochs=10)
 print("done")
