d357808c47d28b1994f2f0f9b0589a5a3996560b
Or Shachar <orsh>
2021-02-16 21:46:45 +0200

    Evaluate & Display Result

---
+++ FakeNewsDetection/Evaluate.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+from SaveLoad import load_metrics
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+import torch
+import Parameters
+import seaborn as sns
+
+
+def display_loss_graph(metric_file_location):
+    train_loss_list, valid_loss_list, global_steps_list = load_metrics(metric_file_location)
+    plt.plot(global_steps_list, train_loss_list, label='Train')
+    plt.plot(global_steps_list, valid_loss_list, label='Valid')
+    plt.xlabel('Global Steps')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+
+
+def evaluate(model, test_loader):
+    y_pred = []
+    y_true = []
+
+    model.eval()
+    with torch.no_grad():
+        for (text_id, title, labels), _ in test_loader:
+
+            labels = labels.type(torch.LongTensor)
+            labels = labels.to(Parameters.DEVICE)
+            title = title.type(torch.LongTensor)
+            title = title.to(Parameters.DEVICE)
+            output = model(title, labels)
+
+            _, output = output
+            y_pred.extend(torch.argmax(output, 1).tolist())
+            y_true.extend(labels.tolist())
+
+    print('Classification Report:')
+    print(classification_report(y_true, y_pred, labels=[1, 0], digits=4))
+
+    cm = confusion_matrix(y_true, y_pred, labels=[1,0])
+    ax = plt.subplot()
+    sns.heatmap(cm, annot=True, ax=ax, cmap='Blues', fmt="d")
+
+    ax.set_title('Confusion Matrix')
+
+    ax.set_xlabel('Predicted Labels')
+    ax.set_ylabel('True Labels')
+
+    ax.xaxis.set_ticklabels(['FAKE', 'REAL'])
+    ax.yaxis.set_ticklabels(['FAKE', 'REAL'])
--- FakeNewsDetection/main.py
+++ FakeNewsDetection/main.py
@@ -1,11 +1,27 @@
 from BERT import BERT
+from SaveLoad import load_checkpoint
+from Evaluate import evaluate, display_loss_graph
+from DatasetPrepare import train_iter
 import Parameters
 from Train import train
 import torch.optim as optim
 
-model = BERT()
-model = model.to(Parameters.DEVICE)
-optimizer = optim.Adam(model.parameters(), lr=2e-4)
 
-train(model=model, optimizer=optimizer, num_epochs=10)
-print("done")
+def train_model():
+    model = BERT()
+    model = model.to(Parameters.DEVICE)
+    optimizer = optim.Adam(model.parameters(), lr=8e-5)
+
+    train(model=model, optimizer=optimizer, num_epochs=10)
+    print("done")
+
+
+def display_result():
+    best_model = BERT().to(Parameters.DEVICE)
+    load_checkpoint(Parameters.MODEL_OUTPUT_FILE, best_model)
+    display_loss_graph(Parameters.METRICS_OUTPUT_FILE)
+    evaluate(best_model, train_iter)
+
+
+train_model()
+display_result()
